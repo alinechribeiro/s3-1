4f0f4d05a2c6345c6cfd70cf7145ee0f610c4188
Aline Chaves Ribeiro <alinechribeiro@gmail.com>
2017-01-23 20:59:56 +0000

    up version 12

--- settings/staging.py
+++ settings/staging.py
@@ -23,7 +23,7 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_3CGVcNJRchvGx7zp17jBIgRD')
 ###################### LARRY ALTEROU ############################
 # Paypal environment variables
 #SITE_URL = 'http://127.0.0.1:8000'
-SITE_URL = 'http://s3-1.heroku.com'
+SITE_URL = 'http://s3-1.herokuapp.com'
 #PAYPAL_NOTIFY_URL = 'https://4d6e916a.ngrok.io/'
-PAYPAL_NOTIFY_URL = 'https://s3-1.heroku.com'
+PAYPAL_NOTIFY_URL = 'https://s3-1.herokuapp.com'
 PAYPAL_RECEIVER_EMAIL = 'alinechribeiro@gmail.com'
